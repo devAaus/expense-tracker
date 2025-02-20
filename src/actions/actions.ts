fce82a8a8f4871622e3fd1cea2632cee1e05a462
Aayush <aausgh@gmail.com>
2025-02-20 20:57:03 +0545

    get data by user

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,17 +1,41 @@
 "use server"
 
 import { prisma } from "@/lib/db"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation";
+
+const authCheck = async () => {
+   // auth check
+   const { isAuthenticated, getUser } = getKindeServerSession();
+   if (!(await isAuthenticated())) {
+      return redirect("/api/auth/login");
+   }
+
+   const user = await getUser();
+   return user;
+}
 
 export async function getExpenses() {
-   return await prisma.expense.findMany()
+   const user = await authCheck();
+   return await prisma.expense.findMany({
+      where: {
+         userId: user.id
+      },
+      orderBy: {
+         createdAt: "desc"
+      }
+   })
 }
 
 export async function addExpense(formData: FormData) {
+   const user = await authCheck();
+
    await prisma.expense.create({
       data: {
          description: formData.get("description") as string,
          amount: Number(formData.get("amount")),
+         userId: user.id
       }
    })
 
@@ -19,6 +43,7 @@ export async function addExpense(formData: FormData) {
 }
 
 export async function deleteExpense(id: number) {
+   await authCheck();
    await prisma.expense.delete({
       where: {
          id
